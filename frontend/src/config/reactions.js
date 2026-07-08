c5fb8e15ee50193bd5f6a441c6b0b78877a18ec2
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-07-07 20:57:39 -0500

    Add dislike reaction option to reactionOptions array

--- frontend/src/config/reactions.js
+++ frontend/src/config/reactions.js
@@ -1,8 +1,10 @@
 export const reactionOptions = [
   { emoji: "\u{1F44D}", key: "like" },
+  { emoji: "\u{1F44E}", key: "dislike" },
   { emoji: "❤️", key: "important" },
   { emoji: "\u{1F602}", key: "light" },
   { emoji: "\u{1F62E}", key: "attention" },
   { emoji: "\u{1F69A}", key: "onRoute" },
   { emoji: "✅", key: "resolved" },
+
 ];
